85578592d4ff42c524edd39bbea56ee0d32e019d
Tyler Cummings <tylercummings@comcast.net>
2021-12-13 19:25:40 -0500

    Update git url for deploy

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:tcummin2/phasmophobia-countup.git master:gh-pages
+git push -f git@github.com:tcummin2/phasmophobia.git master:gh-pages
 
 cd -
